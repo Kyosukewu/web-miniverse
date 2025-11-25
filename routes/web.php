86e0e41922fb0884f71461c519797c56ab0c313f
Jerrywu <jerrywu@tvbs.com.tw>
2025-11-25 10:47:32 +0800

    chore: update to Ver.12

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::get('/export', [DashboardController::class, 'export'])->name('dashboard.export');
